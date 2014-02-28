0f5d2a6818162d125d6bc24010f8fb15d84cbab7
piratefsh <sherminn.chong@gmail.com>
2014-02-28 13:58:42 +0800

    Add links and update content

--- _site/js/main.js
+++ _site/js/main.js
@@ -51,3 +51,16 @@ $(function(){
 
     })
 })
+
+// Maps
+$(function(){
+  function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+})
